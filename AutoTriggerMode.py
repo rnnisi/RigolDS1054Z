c274cabd81a7df03bef903857dbd746a0407b825
rnnisi <69220767+rnnisi@users.noreply.github.com>
2020-08-27 11:02:52 -0700

    Update AutoTriggerMode.py

--- AutoTriggerMode.py
+++ AutoTriggerMode.py
@@ -8,13 +8,14 @@ Generate directory with a csv for each waveform grabbed
 
 import RigolDS1054Z as RS
 
-acqt = input("Enter desired duration of acqusition time: ") 
+acqt = input("Enter desired duration of acqusition time in seconds: ") 
 
 rigol = RS.RigolDS1054Z()
-rigol.Connect()
-rigol.SetupNormCollection(1200)
-rigol.GetParams()
-rigol.setupDAQf()
-rigol.GeneralAuto(float(acqt))
-rigol.autogenCSV()
+rigol.Connect() # establish connection
+rigol.SetupNormCollection(1200) # tell scope we want 1200 points in each waveform
+rigol.GetParams() # get paramters for generation of x data. do this before DAQ in case of crash
+rigol.setupDAQf() # configure output files
+rigol.SetAutoTrig() # trigger needs to be in auto mode
+rigol.GeneralAuto(float(acqt))  # collect waveforms from screen for acqt
+rigol.autogenCSV()  # when acqt is over, or if GeneralAuto encounters a fatal error, generate a CSV for each waveform.
 print("Program finished. Exiting...")
