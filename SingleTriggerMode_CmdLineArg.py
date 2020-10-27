9c0ac64d57cd8dd2e3f5c628a1db549319f3188c
Rebecca Nishide <rnnishide@gmail.com>
2020-10-26 22:50:21 -0700

    Update SingleTriggerMode_CmdLineArg.py

--- SingleTriggerMode_CmdLineArg.py
+++ SingleTriggerMode_CmdLineArg.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Rebecca Nishide, last updated 10/26/2020
+# rnnishide@ucsb.edu
 #
 """
 Pull waveforms, as displayed on screen, from scope as fast as allowed
@@ -8,10 +10,12 @@ args[1] = RunTime in seconds
 args[2] = lxi, or normal. use LXU for faster data acquisition if lxi is installed
 args[3] = trigger level, no spaces, include units V, mV, or uV. (eg. 15.V)
 OPTIONAL: args[4-7] = channel assignments 
+Can support between 1-4 channels. 
+Reads each channel one at at a time before resetting trigger. 
+Not simultaneous. MORE CHANNELS = LONGER RUN TIME
 """
 
 import RigolDS1054Z as RS
-import sys
 
 
 rigol = RS.RigolDS1054Z()
