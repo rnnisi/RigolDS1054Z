f213f61e0ca5aaa6c8dedb1b91ff37b780b4feb3
Rebecca Nishide <rnnishide@gmail.com>
2020-11-18 15:35:49 -0800

    Add files via upload

---
+++ USB_Run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#
+"""
+Pull waveforms, as displayed on screen, from scope as fast as allowed
+Log experiment
+Generate directory with a csv for each waveform grabbed
+args[1] = RunTime in seconds
+args[3] = 'AUTO', 'FORCE_TRIG', or trigger level; no spaces, include units V, mV, or uV. (eg. 15.V)
+OPTIONAL: args[4-7] = channel assignments 
+"""
+
+import RigolDS1054Z as RS
+
+rigol = RS.RigolDS1054Z('USB')					# Use class RigolDS1054Z
+args = rigol.CmdLinArg(3)					# Dictate minimum number of command line arguements acceptable
+rigol.Connect('AutoConnect')				# AutoConnect. This arguement can be changed to scope address for manual connection if desired
+rigol.SetupDAQ()							# Establish settings, allow program to identify self contained objects specific to experiment
+rigol.StartDAQ(args[1])			# Start data acquisition, allow to run until it has surpassed given RunTime
+print("Program finished. Exiting...")
+rigol.Exit()
---
+++ plot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+
+import matplotlib.pyplot as plt
+import numpy
+import sys
+import subprocess
+
+args = sys.argv
+
+exp = args[1]
+
+# open log to get channel info 
+TriggerLog = subprocess.check_output(['ls *TriggerLog.txt'], shell = True)
+trig_file = TriggerLog.decode("utf-8")
+trig_file = trig_file.strip()
+trig_log = open(trig_file, 'r')
+lines = trig_log.readlines()
+trig_log.close()
+raw_chan = lines[0].split(':')
+
+# clean up channel numbers 
+
+list_channels = list(raw_chan[1].split(','))
+for i in range(len(list_channels)):
+	list_channels[i] = list_channels[i].strip()
+
+# read waveforms and store data 
+df = "'Wfm_' + str(exp) + '_Ch_' + str(i) + '.csv'"
+
+for i in list_channels:
+	try:
+		dat = open(eval(df), 'r')
+	except FileNotFoundError:
+		pass
+	raw = str(dat.read())
+	lines = list(raw.split('\n'))
+	time = []
+	volts = []
+	for j in range(1, len(lines)):
+		try:
+			temp = str(lines[j]).split(', ')
+			volts.append(float(temp[1]))
+			time.append(float(temp[0]))
+		except:
+			pass
+# plot data 
+	plt.plot(time, volts, label = "channel " + str(i))
+
+
+plt.legend()
+plt.title("Waveform Set #" + str(exp))
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.show()
