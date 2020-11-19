0614a7bdf1ee8dbd7d9e091b1d53804846f49dd5
Rebecca Nishide <rnnishide@gmail.com>
2020-11-18 16:22:32 -0800

    Update LAN_Run.py

--- LAN_Run.py
+++ LAN_Run.py
@@ -5,15 +5,14 @@ Pull waveforms, as displayed on screen, from scope as fast as allowed
 Log experiment
 Generate directory with a csv for each waveform grabbed
 args[1] = RunTime in seconds
-args[2] = lxi, or normal. use LXU for faster data acquisition if lxi is installed
-args[3] = 'AUTO', 'FORCE_TRIG', or trigger level; no spaces, include units V, mV, or uV. (eg. 15.V)
-OPTIONAL: args[4-7] = channel assignments 
+args[2] = 'AUTO', 'FORCE_TRIG', or trigger level; no spaces, include units V, mV, or uV. (eg. 15.V)
+OPTIONAL: args[3-6] = channel assignments 
 """
 
 import RigolDS1054Z as RS
 
 
-rigol = RS.RigolDS1054Z('LAN')					# Use class RigolDS1054Z
+rigol = RS.RigolDS1054Z('LAN')					# Use class RigolDS1054Z, specify that you want to collect data through local network 
 args = rigol.CmdLinArg(3)					# Dictate minimum number of command line arguements acceptable
 rigol.Connect('AutoConnect')				# AutoConnect. This arguement can be changed to scope address for manual connection if desired
 rigol.SetupDAQ()							# Establish settings, allow program to identify self contained objects specific to experiment
