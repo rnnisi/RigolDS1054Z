c8faf3562268eddffae1bdb4bd10d4624a7edd1f
Rebecca Nishide <rnnishide@gmail.com>
2020-11-23 13:13:02 -0800

    Add files via upload

---
+++ DOWNLOAD.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import DataTransfer as dt
+
+DT= dt.DataTransfer()
+
+DT.DownloadData('192.168.1.24')
---
+++ UPLOAD.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import DataTransfer as dt
+
+DT= dt.DataTransfer()
+
+DT.UploadData()
